8bdb391c51df80688a6a1997e704622d6d9fc190
koneil612 <kellis1286@gmail.com>
2017-02-13 20:51:25 -0600

    Tic_Tac_Toe
    using react

---
+++ src/js/client.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Game from "./components/Game";
+import Board from "./components/Board";
+import Square from "./components/Square";
+
+const app = document.getElementById('app');
+
+
+
+ReactDOM.render(<Game />, app);
